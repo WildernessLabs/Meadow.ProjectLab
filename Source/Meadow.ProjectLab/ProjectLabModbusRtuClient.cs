039691d1ae3b5173b7faee7e210de45c15882ac9
Adrian Stevens <adrian.stevens@gmail.com>
2023-09-20 12:28:56 -0700

    XML comments + minor cleanup

--- Source/Meadow.ProjectLab/ProjectLabModbusRtuClient.cs
+++ Source/Meadow.ProjectLab/ProjectLabModbusRtuClient.cs
@@ -4,8 +4,16 @@ using System.Threading;
 
 namespace Meadow.Devices
 {
+    /// <summary>
+    /// Represents a Modbus RTU client customized for ProjectLab.
+    /// </summary>
     public class ProjectLabModbusRtuClient : ModbusRtuClient
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectLabModbusRtuClient"/> class.
+        /// </summary>
+        /// <param name="port">The serial port for communication.</param>
+        /// <param name="enablePort">The digital output port used for enable control.</param>
         public ProjectLabModbusRtuClient(ISerialPort port, IDigitalOutputPort enablePort)
             : base(port, enablePort)
         {
@@ -15,10 +23,9 @@ namespace Meadow.Devices
             // meadow is not-so-fast, and data will not all get transmitted before the call to the port Write() returns
             PostWriteDelayAction = (m) =>
             {
-                var delay = (int)((1d / port.BaudRate) * port.DataBits * 1000d * m.Length) + 3; // +3 to add just a little extra for clients who are a little slow to turn off the enable pin
+                var delay = (int)(1d / port.BaudRate * port.DataBits * 1000d * m.Length) + 3; // +3 to add just a little extra for clients who are a little slow to turn off the enable pin
                 Thread.Sleep(delay);
             };
         }
     }
-
 }
