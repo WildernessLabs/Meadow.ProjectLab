8500b3a0c8c9a48e4fff8a6f38144d4617f6e197
Adrian Stevens <adrian.stevens@gmail.com>
2024-03-28 15:43:52 -0700

    Fix namespace for Bmi270 fix

--- Source/Meadow.ProjectLab/ProjectLabHardwareBase.cs
+++ Source/Meadow.ProjectLab/ProjectLabHardwareBase.cs
@@ -1,6 +1,6 @@
-﻿using Meadow.Foundation.Sensors.Accelerometers;
-using Meadow.Foundation.Sensors.Atmospheric;
+﻿using Meadow.Foundation.Sensors.Atmospheric;
 using Meadow.Foundation.Sensors.Light;
+using Meadow.Foundation.Sensors.Motion;
 using Meadow.Hardware;
 using Meadow.Logging;
 using Meadow.Modbus;
