87f26945334ab0529c8a3105d2efb2c76453fb08
Adrian Stevens <adrian.stevens@gmail.com>
2024-07-01 11:20:13 -0700

    Fix demo app

--- Source/ProjectLab_Demo/MeadowApp.cs
+++ Source/ProjectLab_Demo/MeadowApp.cs
@@ -10,7 +10,7 @@ namespace ProjectLab_Demo;
 
 // Change F7FeatherV2 to F7FeatherV1 if using Feather V1 Meadow boards
 // Change to F7CoreComputeV2 for Project Lab V3.x
-public class MeadowApp : ProjectLabCoreComputeAppBase
+public class MeadowApp : ProjectLabCoreComputeApp
 {
     private DisplayController? displayController;
 
