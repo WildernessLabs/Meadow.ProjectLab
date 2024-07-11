633c9aa9b1d27c793aa5f79994bb5daff01e0c82
Adrian Stevens <adrian.stevens@gmail.com>
2024-07-11 15:55:20 -0700

    Release 1.12.8

--- Source/Meadow.ProjectLab/ProjectLabHardwareV1.cs
+++ Source/Meadow.ProjectLab/ProjectLabHardwareV1.cs
@@ -27,12 +27,6 @@ public class ProjectLabHardwareV1 : ProjectLabHardwareBase
     private readonly string revision = "v1.x";
 
     /// <inheritdoc/>
-    public sealed override II2cBus I2cBus { get; }
-
-    /// <inheritdoc/>
-    public sealed override ISpiBus SpiBus { get; }
-
-    /// <inheritdoc/>
     public override IButton UpButton { get; }
 
     /// <inheritdoc/>
@@ -51,17 +45,9 @@ public class ProjectLabHardwareV1 : ProjectLabHardwareBase
     public override IRgbPwmLed? RgbLed => GetRgbLed();
 
     internal ProjectLabHardwareV1(IF7FeatherMeadowDevice device, II2cBus i2cBus)
+        : base(device, i2cBus)
     {
         _device = device;
-        I2cBus = i2cBus;
-
-        Logger?.Trace("Instantiating SPI Bus");
-        SpiBus = Resolver.Device.CreateSpiBus(
-            device.Pins.SCK,
-            device.Pins.COPI,
-            device.Pins.CIPO,
-            new Frequency(48000, Frequency.UnitType.Kilohertz));
-        Logger?.Trace("SPI Bus up");
 
         Logger?.Trace("Instantiating buttons");
         LeftButton = GetPushButton(device.Pins.D10);
@@ -78,13 +64,13 @@ public class ProjectLabHardwareV1 : ProjectLabHardwareBase
         {
             Logger?.Trace("Instantiating display");
 
-            var chipSelectPort = DisplayHeader.Pins.CS.CreateDigitalOutputPort();
-            var dcPort = DisplayHeader.Pins.DC.CreateDigitalOutputPort();
-            var resetPort = DisplayHeader.Pins.RST.CreateDigitalOutputPort();
+            var chipSelectPort = DisplayHeader.Pins.DISPLAY_CS.CreateDigitalOutputPort();
+            var dcPort = DisplayHeader.Pins.DISPLAY_DC.CreateDigitalOutputPort();
+            var resetPort = DisplayHeader.Pins.DISPLAY_RST.CreateDigitalOutputPort();
             Thread.Sleep(50);
 
             _display = new St7789(
-                spiBus: SpiBus,
+                spiBus: DisplayHeader.SpiBusDisplay,
                 chipSelectPort: chipSelectPort,
                 dataCommandPort: dcPort,
                 resetPort: resetPort,
@@ -92,7 +78,7 @@ public class ProjectLabHardwareV1 : ProjectLabHardwareBase
                 colorMode: ColorMode.Format16bppRgb565)
             {
                 SpiBusMode = SpiClockConfiguration.Mode.Mode3,
-                SpiBusSpeed = new Frequency(48000, Frequency.UnitType.Kilohertz)
+                SpiBusSpeed = new Frequency(24000, Frequency.UnitType.Kilohertz)
             };
             ((St7789)_display).SetRotation(RotationType._270Degrees);
 
@@ -152,7 +138,7 @@ public class ProjectLabHardwareV1 : ProjectLabHardwareBase
             new PinMapping
             {
                 new PinMapping.PinAlias(MikroBusConnector.PinNames.AN, _device.Pins.A00),
-                // no RST connected
+                // no DISPLAY_RST connected
                 new PinMapping.PinAlias(MikroBusConnector.PinNames.CS, _device.Pins.D14),
                 new PinMapping.PinAlias(MikroBusConnector.PinNames.SCK, _device.Pins.SCK),
                 new PinMapping.PinAlias(MikroBusConnector.PinNames.CIPO, _device.Pins.CIPO),
@@ -259,12 +245,13 @@ public class ProjectLabHardwareV1 : ProjectLabHardwareBase
            nameof(Display),
             new PinMapping
             {
-                new PinMapping.PinAlias(DisplayConnector.PinNames.CS, _device.Pins.A03),
-                new PinMapping.PinAlias(DisplayConnector.PinNames.RST, _device.Pins.A05),
-                new PinMapping.PinAlias(DisplayConnector.PinNames.DC, _device.Pins.A04),
-                new PinMapping.PinAlias(DisplayConnector.PinNames.CLK, _device.Pins.SCK),
-                new PinMapping.PinAlias(DisplayConnector.PinNames.COPI, _device.Pins.COPI),
-            });
+                new PinMapping.PinAlias(DisplayConnector.PinNames.DISPLAY_CS, _device.Pins.A03),
+                new PinMapping.PinAlias(DisplayConnector.PinNames.DISPLAY_RST, _device.Pins.A05),
+                new PinMapping.PinAlias(DisplayConnector.PinNames.DISPLAY_DC, _device.Pins.A04),
+                new PinMapping.PinAlias(DisplayConnector.PinNames.DISPLAY_CLK, _device.Pins.SCK),
+                new PinMapping.PinAlias(DisplayConnector.PinNames.DISPLAY_COPI, _device.Pins.COPI),
+            },
+            new SpiBusMapping(_device, _device.Pins.SCK, _device.Pins.COPI, _device.Pins.CIPO));
     }
 
     /// <inheritdoc/>
--- Source/Meadow.ProjectLab/ProjectLabModbusRtuClient.cs
+++ Source/Meadow.ProjectLab/ProjectLabModbusRtuClient.cs
@@ -2,30 +2,29 @@
 using Meadow.Modbus;
 using System.Threading;
 
-namespace Meadow.Devices
+namespace Meadow.Devices;
+
+/// <summary>
+/// Represents a Modbus RTU client customized for ProjectLab.
+/// </summary>
+public class ProjectLabModbusRtuClient : ModbusRtuClient
 {
     /// <summary>
-    /// Represents a Modbus RTU client customized for ProjectLab.
+    /// Initializes a new instance of the <see cref="ProjectLabModbusRtuClient"/> class.
     /// </summary>
-    public class ProjectLabModbusRtuClient : ModbusRtuClient
+    /// <param name="port">The serial port for communication.</param>
+    /// <param name="enablePort">The digital output port used for enable control.</param>
+    public ProjectLabModbusRtuClient(ISerialPort port, IDigitalOutputPort enablePort)
+        : base(port, enablePort)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ProjectLabModbusRtuClient"/> class.
-        /// </summary>
-        /// <param name="port">The serial port for communication.</param>
-        /// <param name="enablePort">The digital output port used for enable control.</param>
-        public ProjectLabModbusRtuClient(ISerialPort port, IDigitalOutputPort enablePort)
-            : base(port, enablePort)
-        {
-            // this forces meadow to compile the serial pipeline.  Without it, there's a big delay on sending the first byte
-            PostOpenAction = () => { port.Write(new byte[] { 0x00 }); };
+        // this forces meadow to compile the serial pipeline.  Without it, there's a big delay on sending the first byte
+        PostOpenAction = () => { port.Write(new byte[] { 0x00 }); };
 
-            // meadow is not-so-fast, and data will not all get transmitted before the call to the port Write() returns
-            PostWriteDelayAction = (m) =>
-            {
-                var delay = (int)(1d / port.BaudRate * port.DataBits * 1000d * m.Length) + 3; // +3 to add just a little extra for clients who are a little slow to turn off the enable pin
-                Thread.Sleep(delay);
-            };
-        }
+        // meadow is not-so-fast, and data will not all get transmitted before the call to the port Write() returns
+        PostWriteDelayAction = (m) =>
+        {
+            var delay = (int)(1d / port.BaudRate * port.DataBits * 1000d * m.Length) + 3; // +3 to add just a little extra for clients who are a little slow to turn off the enable pin
+            Thread.Sleep(delay);
+        };
     }
 }
